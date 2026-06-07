3535af8e1ea61f83768a594243dd6c27ca2ce3e1
Shengwei Xiong <contact@shiroharu.eu.org>
2026-06-07 13:33:41 +0700

    fix: self-host document

--- app/backup/page.tsx
+++ app/backup/page.tsx
@@ -8,7 +8,7 @@ import { HeaderCanvas } from '@/components/HeaderCanvas';
 import { supabase } from '@/lib/supabase';
 import NotFound from '../not-found';
 
-export default function GuidePage() {
+export default function BackupPage() {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
@@ -38,8 +38,16 @@ export default function GuidePage() {
       <div className="page-body">
         <div className="container">
           <main className="arcantia-homepage">
-            <h1 className="section-title"><span>Server Guide</span></h1>
-              <iframe src="/documents/guide.pdf" title="Server Guide" style={{ width: '100%', height: '100vh', border: 'none', minHeight: '600px' }} />
+            <h1 className="section-title"><span>Server Backup</span></h1>
+             <div className="panel arcantia-page" style={{ backgroundColor: '#fefefe' }}>
+              <div className="panel-inner page-content post no-image">
+                <section className="post-full-content">
+                  <div className="post-content">
+                    <iframe src="https://drive.google.com/embeddedfolderview?id=1IRoOkXI8TsICJ60zUIRWu2MO5p73ANqQ#list" title="Server Backup" style={{ width: '100%', height: '100vh', border: 'none', minHeight: '600px' }} />
+                  </div>
+                </section>
+              </div>
+            </div>
           </main>
         </div>
       </div>
